b80cdfa7f182e4404702dcd2156af16ae9eb8de9
Rafael Felix <felix.rafael@gmail.com>
2013-02-25 21:47:33 -0300

    Add cielo test specs
    Cielo provides two test specs, this commit introduces a simplier
    implementation of these ones.

--- lib/cielo.rb
+++ lib/cielo.rb
@@ -2,6 +2,8 @@ require "cielo/version"
 
 module Cielo
   autoload :Spec, "cielo/spec"
+  autoload :ShopTestSpec, "cielo/spec"
+  autoload :CieloTestSpec, "cielo/spec"
   autoload :Transaction, "cielo/transaction"
   autoload :Request, "cielo/request"
   autoload :Response, "cielo/response"
--- lib/cielo/spec.rb
+++ lib/cielo/spec.rb
@@ -12,4 +12,24 @@ module Cielo
       Cielo::Transaction.new(self, &block)
     end
   end
+  
+  class ShopTestSpec < Spec
+    def initialize
+      super(
+        number: 1006993069,
+        token: "25fbb99741c739dd84d7b06ec78c9bac718838630f30b112d033ce2e621b34f3",
+        endpoint: "https://qasecommerce.cielo.com.br/servicos/ecommwsec.do"
+      )
+    end
+  end
+  
+  class CieloTestSpec < Spec
+    def initialize
+      super(
+        number: 1001734898,
+        token: "e84827130b9837473681c2787007da5914d6359947015a5cdb2b8843db0fa832",
+        endpoint: "https://qasecommerce.cielo.com.br/servicos/ecommwsec.do"
+      )
+    end
+  end
 end
